3e5b9d6f643a127f2e928bcc348340dbc0eb426c
Webdegre@6 <webdegredg@gmail.com>
2024-07-12 12:14:40 +0530

    first commit

--- app/page.js
+++ app/page.js
@@ -1,100 +1,11 @@
-'use client'
 import Image from "next/image";
-import { useState ,useEffect } from "react";
-import Swipe from "react-easy-swipe";
-import { AiOutlineLeft, AiOutlineRight } from "react-icons/ai";
 
-/**
- * Carousel component for nextJS and Tailwind.
- * Using external library react-easy-swipe for swipe gestures on mobile devices (optional)
- *
- * @param images - Array of images with src and alt attributes
- * @returns React component
- */
-const Home = ()=> {
-
-  const images = [
-    { id: 1, src: "https://allroundclub.com/blog/wp-content/uploads/2021/10/how-to-draw-pikachu.png", alt: "Image 1" },
-    { id: 2, src: "https://www.denofgeek.com/wp-content/uploads/2021/04/Pikachu.png?fit=1920%2C1080", alt: "Image 2" },
-    { id: 3, src: "https://static.wikia.nocookie.net/thepokemonshow/images/8/80/20101007155439%21Ash_Pikachu.png/revision/latest?cb=20140425213328", alt: "Image 3" },
-    { id: 4, src: "https://allroundclub.com/blog/wp-content/uploads/2021/10/how-to-draw-pikachu.png", alt: "Image 4"},
-  ]
-  const [currentSlide, setCurrentSlide] = useState(0);
-
-  const handleNextSlide = () => {
-    let newSlide = currentSlide === images.length - 1 ? 0 : currentSlide + 1;
-    setCurrentSlide(newSlide);
-  };
-
-  const handlePrevSlide = () => {
-    let newSlide = currentSlide === 0 ? images.length - 1 : currentSlide - 1;
-    setCurrentSlide(newSlide);
-  };
-
-  
-
-  
-
-  useEffect(() => {
-    const intervalId = setInterval(handleNextSlide, 4000);
-
-    // Clean up the interval on component unmount
-    return () => clearInterval(intervalId);
-  }, [currentSlide, images.length]);
-
+export default function Home() {
   return (
-    <div className="relative">
-      <AiOutlineLeft
-        onClick={handlePrevSlide}
-        className="absolute left-0 m-auto text-5xl inset-y-1/2 cursor-pointer text-gray-400 z-20"
-      />
-      <div className="w-full h-[50vh] flex overflow-hidden relative m-auto">
-        <Swipe
-          onSwipeLeft={handleNextSlide}
-          onSwipeRight={handlePrevSlide}
-          className="relative z-10 w-full h-full"
-        >
-          {images.map((image, index) => {
-            if (index === currentSlide) {
-              return (
-                <Image
-                  key={image.id}
-                  image={image}
-                  src={image.src}
-                  alt={image.alt}
-                  layout="fill"
-                  objectFit="contain"
-                  className="animate-fadeIn"
-                />
-              );
-            }
-          })}
-        </Swipe>
-      </div>
-      <AiOutlineRight
-        onClick={handleNextSlide}
-        className="absolute right-0 m-auto text-5xl inset-y-1/2 cursor-pointer text-gray-400 z-20"
-      />
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      
 
-      <div className="relative flex justify-center p-2">
-        {images.map((_, index) => {
-          return (
-            <div
-              className={
-                index === currentSlide
-                  ? "h-4 w-4 bg-gray-700 rounded-full mx-2 mb-2 cursor-pointer"
-                  : "h-4 w-4 bg-gray-300 rounded-full mx-2 mb-2 cursor-pointer"
-              }
-              key={index}
-              onClick={() => {
-                setCurrentSlide(index);
-              }}
-            />
-          );
-        })}
-      </div>
-    </div>
+     
+    </main>
   );
 }
-
-export default Home;
--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    images: {
-        domains: ['allroundclub.com','www.denofgeek.com','www.denofgeek.com','static.wikia.nocookie.net','static.wikia.nocookie.net'],
-      },
-};
+const nextConfig = {};
 
 export default nextConfig;
